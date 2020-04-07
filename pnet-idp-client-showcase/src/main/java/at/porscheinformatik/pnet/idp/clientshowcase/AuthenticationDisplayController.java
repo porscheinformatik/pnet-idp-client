806ca0f0e9bf7d90602d32d3c4c46ae4ab292c6a
Daniel Furtlehner <daniel.furtlehner@porscheinformatik.com>
2020-04-07 10:41:24 +0200

    Add thymeleaf templates
    Instead of simply showing the json data, we render a simple page that
    displays the data. This makes it possible to extend the functionality of
    the app later on (e.g. logout,...)

--- pnet-idp-client-showcase/src/main/java/at/porscheinformatik/pnet/idp/clientshowcase/AuthenticationDisplayController.java
+++ pnet-idp-client-showcase/src/main/java/at/porscheinformatik/pnet/idp/clientshowcase/AuthenticationDisplayController.java
@@ -3,27 +3,44 @@
  */
 package at.porscheinformatik.pnet.idp.clientshowcase;
 
-import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import at.porscheinformatik.idp.openidconnect.PartnerNetOpenIdConnectUser;
 
 /**
  * @author Daniel Furtlehner
  */
-@RestController
-@RequestMapping("/data/authentication")
+@Controller
+@RequestMapping("/")
 public class AuthenticationDisplayController
 {
+    private final ObjectMapper objectMapper;
 
-    @GetMapping(value = {"", "/"}, produces = MediaType.APPLICATION_JSON_VALUE)
-    public AuthenticationDTO getAuthentication()
+    @Autowired
+    public AuthenticationDisplayController(ObjectMapper objectMapper)
     {
-        return buildAuthentication();
+        super();
+
+        this.objectMapper = objectMapper;
+    }
+
+    @GetMapping
+    public String getAuthentication(Model model) throws JsonProcessingException
+    {
+        AuthenticationDTO dto = buildAuthentication();
+
+        model.addAttribute("authData", objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(dto));
+
+        return "index";
     }
 
     private AuthenticationDTO buildAuthentication()
