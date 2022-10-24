54026b5641b00e2d76ffd65e4748626c747c3fd5
Daniel Furtlehner <daniel.furtlehner@porscheinformatik.com>
2022-10-24 15:30:52 +0200

    Remove serialization fix
    The fix is not needed anymore, as the class in question is already
    serializable in the newest spring-security version.

--- pnet-idp-client-saml2/src/main/java/at/porscheinformatik/idp/saml2/PartnerNetSaml2ImportSelector.java
+++ pnet-idp-client-saml2/src/main/java/at/porscheinformatik/idp/saml2/PartnerNetSaml2ImportSelector.java
@@ -2,17 +2,12 @@ package at.porscheinformatik.idp.saml2;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.context.annotation.ImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
-import at.porscheinformatik.idp.saml2.workaround.PartnerNetSaml2SerializationFixConfiguration;
-
 public class PartnerNetSaml2ImportSelector implements ImportSelector
 {
-    public static final String REGISTER_SERIALIZATION_FIX_ATTRIBUTE = "registerSerializationFix";
-
     /**
      *
      */
@@ -21,16 +16,6 @@ public class PartnerNetSaml2ImportSelector implements ImportSelector
     {
         List<String> importClasses = new ArrayList<>();
         importClasses.add(PartnerNetSaml2Configuration.class.getName());
-
-        Map<String, Object> metadata =
-            importingClassMetadata.getAnnotationAttributes(EnablePartnerNetSaml2.class.getName());
-
-        Boolean registerFix = (Boolean) metadata.getOrDefault(REGISTER_SERIALIZATION_FIX_ATTRIBUTE, Boolean.FALSE);
-
-        if (registerFix.booleanValue())
-        {
-            importClasses.add(PartnerNetSaml2SerializationFixConfiguration.class.getName());
-        }
 
         return importClasses.toArray(String[]::new);
     }
