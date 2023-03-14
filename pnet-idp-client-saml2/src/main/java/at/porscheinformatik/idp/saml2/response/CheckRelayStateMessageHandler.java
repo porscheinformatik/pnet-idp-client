cd64c91166968ff21cacb30d0373a01993b10eee
Kareem <abd.aljeiroudi@porscheinformatik.com>
2023-03-14 11:57:06 +0100

    Check relay state in SAML Response to avoid CSRF
    (cherry picked from commit 579f82f66ed5cc92072549386cb2b7fafb8cb61e)

---
+++ pnet-idp-client-saml2/src/main/java/at/porscheinformatik/idp/saml2/response/CheckRelayStateMessageHandler.java
@@ -0,0 +1,32 @@
+package at.porscheinformatik.idp.saml2.response;
+
+import static at.porscheinformatik.idp.saml2.Saml2ResponseProcessor.*;
+
+import java.util.Objects;
+
+import org.opensaml.messaging.context.MessageContext;
+import org.opensaml.messaging.handler.MessageHandlerException;
+
+public class CheckRelayStateMessageHandler extends AbstractSimpleMessageHandler
+{
+
+    @Override
+    public void invoke(MessageContext messageContext) throws MessageHandlerException
+    {
+        Saml2AuthenticationTokenContext authenticationContext =
+            messageContext.getSubcontext(Saml2AuthenticationTokenContext.class);
+        String responseRelayState = authenticationContext
+            .getResponseRelayState()
+            .orElseThrow(() -> new MessageHandlerException("Relay state is missing in response."));
+        String requestedRelayState = authenticationContext
+            .getRequestedRelayState()
+            .orElseThrow(
+                () -> new MessageHandlerException("Requested relay state is missing."));
+
+        if (!Objects.equals(responseRelayState, requestedRelayState))
+        {
+            throw new MessageHandlerException("Requested relay state doesn't match relay state in response");
+        }
+    }
+
+}
