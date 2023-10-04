70477bbc4c1cf1fce207ffe320148e4e220769d6
David Schöninger <david.schoeninger.ext@porscheinformatik.com>
2023-10-04 13:34:37 +0200

    Add possibility to set custom state for OIDC

---
+++ pnet-idp-client-openidconnect/src/test/java/at/porscheinformatik/idp/openidconnect/PartnerNetOpenIdConnectStateUtilsTest.java
@@ -0,0 +1,48 @@
+package at.porscheinformatik.idp.openidconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PartnerNetOpenIdConnectStateUtilsTest
+{
+    @Test
+    public void testBuildState()
+    {
+        assertThrows(IllegalArgumentException.class,
+            () -> PartnerNetOpenIdConnectStateUtils.buildState(null, "cst"));
+        assertThrows(IllegalArgumentException.class,
+            () -> PartnerNetOpenIdConnectStateUtils.buildState("", "cst"));
+
+        assertEquals(encodeBase64("rnd"),
+            PartnerNetOpenIdConnectStateUtils.buildState("rnd", null));
+        assertEquals(encodeBase64("rnd:"),
+            PartnerNetOpenIdConnectStateUtils.buildState("rnd", ""));
+        assertEquals(encodeBase64("rnd:cst"),
+            PartnerNetOpenIdConnectStateUtils.buildState("rnd", "cst"));
+    }
+
+    @Test
+    public void testGetCustomState()
+    {
+        assertNull(PartnerNetOpenIdConnectStateUtils.getCustomState((String) null));
+        assertNull(PartnerNetOpenIdConnectStateUtils.getCustomState(new MockHttpServletRequest()));
+        assertNull(PartnerNetOpenIdConnectStateUtils.getCustomState(encodeBase64("rnd")));
+
+        assertEquals("",
+            PartnerNetOpenIdConnectStateUtils.getCustomState(encodeBase64("rnd:")));
+        assertEquals("cst",
+            PartnerNetOpenIdConnectStateUtils.getCustomState(encodeBase64("rnd:cst")));
+        assertEquals("cst:with:delimiter",
+            PartnerNetOpenIdConnectStateUtils.getCustomState(encodeBase64("rnd:cst:with:delimiter")));
+    }
+
+    private String encodeBase64(String str)
+    {
+        return Base64.getEncoder().encodeToString(str.getBytes(StandardCharsets.UTF_8));
+    }
+}
